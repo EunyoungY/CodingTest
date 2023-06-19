e8358c513e8a6815e99ca515ba6b8132277919be
EunyoungY <soj6182@gmail.com>
2023-06-19 19:12:57 +0900

    greedy 1

--- dfs/baekjoon/p1339.py
+++ dfs/baekjoon/p1339.py
@@ -1,33 +1,24 @@
 import sys
 n = int(input())
 
-# dic = [] #TODO
-dic = {}
+dic = {} # dic = [] #TODO
 words = []
 for i in range(n):
     word = input()
     words.append(word)
-    for j in range(len(word)):
+    for j in range(len(word)): 
         if word[j] not in dic:
-            # dic.update({word[j]: len(word)-(j+1)})
-            dic[word[j]] = len(word)-(j+1)
+            dic[word[j]] = 10**(len(word)-j-1)  
         else:
-            dic[word[j]] = max(dic[word[j]], len(word)-(j+1))
+            dic[word[j]] += 10**(len(word)-j-1)
 
-sorted_dic = sorted(dic.items(), key=lambda x: x[1], reverse=True)  # TODO
-print(sorted_dic)
+dic = sorted(dic.items(), key = lambda x: x[1], reverse=True)
 
-
-dic = {}
-for i in range(len(sorted_dic)):
-    char, val = sorted_dic[i]
-    dic[char] = 9-i
-print(dic)
-
+num = 9
 answer = 0
-for word in words:
-    for i in range(len(word)):
-        answer += dic[word[i]]*(10**(len(word)-i-1))
+for i in dic: 
+    answer += i[1]*num
+    num-=1 
 print(answer)
 
 
