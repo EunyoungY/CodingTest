bf5bc7f8e8f9d51362421b235e9198d47e076df9
EunyoungY <soj6182@gmail.com>
2023-06-30 23:12:06 +0900

    binary search 2

--- baekjoon/p2805.py
+++ baekjoon/p2805.py
@@ -1,18 +1,20 @@
 n, m = map(int, input().split())
-trees = list(map(int,input().split()))
+trees = list(map(int, input().split()))
+
 
 def search():
-    answer = 0
-    start = 0
+    start = 1
     end = max(trees)
-    while start<=end: 
+    while start <= end:
+        answer = 0
         mid = (start+end)//2
         for tree in trees:
-            answer += tree-mid
+            answer += max(tree-mid,0)
         if answer >= m:
             start = mid+1
         else:
             end = mid-1
-    return answer
+    return end
+
 
 print(search())
