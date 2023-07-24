03976ab58d93672a62b3d6b08827e15a854ef953
EunyoungY <soj6182@gmail.com>
2023-07-24 18:51:19 +0900

    review8

---
+++ baekjoon/p10431-1.py
@@ -0,0 +1,13 @@
+p = int(input())
+for _ in range(p): 
+    arr = list(map(int, input().split()))
+    cnt, i = 0,0
+    for i in range(1, 20):
+        for j in range(i+1, 21):
+            if arr[i] > arr[j]: 
+                arr[i],arr[j] = arr[j], arr[i]
+                cnt+=1
+    print(arr[0], cnt)
+            
+        
+        
