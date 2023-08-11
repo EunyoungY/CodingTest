c9c9c02f9298a02674232a71201836796326ec8e
EunyoungY <soj6182@gmail.com>
2023-08-11 23:03:08 +0900

    greedy1

--- baekjoon/p21758.py
+++ baekjoon/p21758.py
@@ -2,22 +2,30 @@ n = int(input())
 arr = list(map(int, input().split()))
 ans = 0
 arrLen = len(arr)
+arrSum = sum(arr)
 
-toRight = [0]
-toLeft = [0]
 if arrLen == 3:
     print(max(arr)*2)
     exit()
-for i in range(1, arrLen):
-    toRight.append(toRight[i-1]+arr[i])
+# 벌 벌 벌통
+for i in range(1, arrLen-1):
+    ans = max(ans, sum(arr[1:])-arr[i] +sum(arr[i+1:])) 
+    # print(1, i, ans, sum(arr[1:])-arr[i], sum(arr[i+1:]))
 
-rArr = list(reversed(arr))
-# print(rArr)
+# 벌 벌통 벌
 for i in range(1, arrLen):
-    toLeft.append(toLeft[i-1]+rArr[i])
+    ans = max(ans, sum(arr[1:i])+sum(arr[i:-1]))
+    # print(2, i, ans,  sum(arr[1:i]), sum(arr[i:-1]))
     
-for i in range(1, arrLen):
-    ans = max(ans, toRight[-1]-arr[i] + toRight[-1]-toRight[i], toLeft[-1]-arr[i] + toLeft[-1]-toLeft[i],)
+# 벌통 벌 벌
+arr = list(reversed(arr))
+for i in range(1, arrLen-1):
+    ans = max(ans, sum(arr[1:])-arr[i] +sum(arr[i+1:])) 
+    # print(3, i, ans, sum(arr[1:])-arr[i], sum(arr[i+1:]))
 
-print( ans) 
-# print(list(reversed(toLeft)))
+print(ans)
+
+'''
+7
+9 9 4 1 4 9 9
+'''
