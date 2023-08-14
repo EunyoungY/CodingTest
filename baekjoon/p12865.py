21d32ebb12ad190eec750f23737b3eb5b56fc088
EunyoungY <soj6182@gmail.com>
2023-08-14 23:38:14 +0900

    dp 1

---
+++ baekjoon/p12865.py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+
+knapsack = [[0]*(k+1) for _ in range(n+1)]
+for i in range(1, n+1):
+    weight, value = map(int, input().split())
+    for j in range(1, k+1):
+        if j < weight:
+            knapsack[i][j] = knapsack[i-1][j]
+        else:
+            knapsack[i][j] = max(knapsack[i-1][j], knapsack[i-1][j-weight]+value)
+            
+# print(*knapsack)
+print(knapsack[n][k])
